a5bd8c7d78fc0400067280fb515b26e5531782da
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-04-29 22:17:00 +0600

    banner section Done

---
+++ src/app/components/banner.jsx
@@ -0,0 +1,19 @@
+import banner from '@/asset/banner.jpg'
+import { MdOutlineArrowBack } from 'react-icons/md';
+function Bannerpage () {
+  return (
+    <div className='h-screen items-center text-center flex flex-col bg-cover mb-30 bg-center' style={{backgroundImage : `url(${banner.src})`}}>
+
+        
+        <div className='my-auto space-y-3'>
+          <h1 className='font-bold text-3xl  '> Find Your Perfect Qurbani Animal Today</h1>
+          <p className='text-[15px] font-bold '>Bangladesh’s Smart Qurbani Livestock Platform</p>
+        <button className='btn mx-auto btn-warning text-white font-semibold flex items-center'>  <MdOutlineArrowBack size={20}/> Buy Now</button>
+
+          </div>
+      </div>
+
+  )
+}
+
+export default Bannerpage 
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -7,7 +7,7 @@ function Notfoundpage() {
     <div className=" flex flex-col justify-center text-center h-screen ">
       <div className="my-auto space-y-3">
         <p className="font-extrabold  text-3xl text-green-900">This URL is Invalid <br />  <span className="font-semibold text-2xl text-green-900">Please Enter Right URL</span></p>
-      <Link href={'/'}> <button className="btn items-center btn-secondary"> <MdOutlineArrowBack size={20} /> Back Home</button></Link>
+      <Link href={'/'}> <button className="btn flex items-center btn-secondary"> <MdOutlineArrowBack size={20} /> Back Home</button></Link>
 
       </div>
       
