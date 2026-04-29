96cadd7d7be1471bb7b8137786bbb6a7c8fdcfd8
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-04-29 21:35:58 +0600

    Project setup & banner footer section Done

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbarpage from '../components/navbar'
+import Footerpage from '../components/footerpage'
+
+function nainlayoutpage ( { children } ) {
+  return (
+    <>
+
+
+<Navbarpage></Navbarpage>
+
+{children}
+
+
+<Footerpage></Footerpage>
+
+
+   
+
+
+
+
+
+    </>
+
+  
+
+
+
+    
+  )
+}
+
+export default nainlayoutpage 
